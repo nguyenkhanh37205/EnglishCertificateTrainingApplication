e30665e27b967e2169afc4d2883e2ad2f8598933
Phamvankien88 <vankien178v@gmail.com>
2024-12-24 11:04:46 +0700

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -4,14 +4,15 @@ go
 use Ung_Dung_Dao_Tao_Chung_Chi_Tieng_Anh
 go
 
--- Tạo bảng Người dùng
-CREATE TABLE NguoiDung (
-    MaNguoiDung INT PRIMARY KEY IDENTITY(1,1),
-    HoTen VARCHAR(100) NOT NULL,
-    Email VARCHAR(100) UNIQUE NOT NULL,
-    MatKhau VARCHAR(255) NOT NULL, 
-    VaiTro NVARCHAR(100) NOT NULL CHECK(VaiTro IN(N'Học viên',N'Giảng viên',N'Quản trị viên')), 
-    SoDienThoai VARCHAR(15) NOT NULL, 
+-- Tạo bảng TaiKhoan
+CREATE TABLE TaiKhoan (
+    MaTaiKhoan INT PRIMARY KEY IDENTITY(1,1),
+    HoTen VARCHAR(50) NOT NULL,
+    GioiTinh CHAR(3) NOT NULL CHECK(GioiTinh IN(N'Nam',N'Nữ'))
+    Email VARCHAR(30) UNIQUE NOT NULL,
+    MatKhau VARCHAR(50) NOT NULL, 
+    VaiTro NVARCHAR(100) NOT NULL CHECK(VaiTro IN(N'Người dùng',N'Quản trị viên',N'Chuyên viên')), 
+    SoDienThoai VARCHAR(10) NOT NULL, 
     NgaySinh DATE NOT NULL,
     NgayDangKy DATETIME DEFAULT CURRENT_TIMESTAMP
 );
@@ -49,30 +50,31 @@ CREATE TABLE BaiKiemTra (
 CREATE TABLE KetQuaKiemTra (
     MaKetQua INT PRIMARY KEY IDENTITY(1,1),
     MaBaiKiemTra INT NOT NULL,
-    MaNguoiDung INT NOT NULL,
+    MaTaiKhoan INT NOT NULL,
     DiemSo DECIMAL(5, 2) NOT NULL,
     NgayLamBai DATETIME NOT NULL,
     FOREIGN KEY (MaBaiKiemTra) REFERENCES BaiKiemTra(MaBaiKiemTra),
-    FOREIGN KEY (MaNguoiDung) REFERENCES NguoiDung(MaNguoiDung)
+    FOREIGN KEY (MaTaiKhoan) REFERENCES TaiKhoan(MaTaiKhoan)
 );
 
 -- Tạo bảng Chứng chỉ
 CREATE TABLE ChungChi (
     MaChungChi INT PRIMARY KEY IDENTITY(1,1),
-    MaNguoiDung INT NOT NULL,
+    MaTaiKhoan INT NOT NULL,
     MaKhoaHoc INT NOT NULL,
     NgayCap DATE NOT NULL,
-    FOREIGN KEY (MaNguoiDung) REFERENCES NguoiDung(MaNguoiDung),
+    Theloai String NOT NULL,
+    FOREIGN KEY (MaTaiKhoan) REFERENCES NguoiDung(MaTaiKhoan),
     FOREIGN KEY (MaKhoaHoc) REFERENCES KhoaHoc(MaKhoaHoc)
 );
 
 -- Tạo bảng Đăng ký khóa học
 CREATE TABLE DangKyKhoaHoc (
     MaDangKy INT PRIMARY KEY IDENTITY(1,1),
-    MaNguoiDung INT NOT NULL,
+    MaTaiKhoan INT NOT NULL,
     MaKhoaHoc INT NOT NULL,
     NgayDangKy DATETIME DEFAULT CURRENT_TIMESTAMP,
     TrangThai NVARCHAR(100) CHECK(TrangThai IN(N'Đang học', N'Hoàn thành', N'Hủy bỏ')),
-    FOREIGN KEY (MaNguoiDung) REFERENCES NguoiDung(MaNguoiDung),
+    FOREIGN KEY (MaTaiKhoan) REFERENCES TaiKhoan(MaTaiKhoan),
     FOREIGN KEY (MaKhoaHoc) REFERENCES KhoaHoc(MaKhoaHoc)
 );
